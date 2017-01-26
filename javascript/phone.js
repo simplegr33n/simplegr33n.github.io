f46efefb4452d058adea2a55b930387ec56a4ef0
simplegr33n <gcgolda@gmail.com>
2017-01-26 14:30:48 -0800

    added github link

---
+++ javascript/phone.js
@@ -0,0 +1,17 @@
+
+<SCRIPT LANGUAGE="JavaScript">
+<!-- hide script from old browsers
+
+
+
+$("#home-button-image").click(function() {
+    $("#phone").animate({
+        width: '20px',
+        height: '20px'
+    }, 1000);
+});
+
+
+
+// end hiding script from old browsers -->
+</SCRIPT>
